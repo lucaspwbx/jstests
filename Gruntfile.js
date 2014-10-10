2893742e167e568ab6370782b4e1a16b68b213f6
Lucas Weiblen <lucasweiblen@gmail.com>
2014-10-10 15:15:35 -0300

    fix gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,16 @@ module.exports = function(grunt) {
   'use strict';
 
   grunt.initConfig({
+    connect: {
+      test: {
+        port: 8000
+      }
+    },
     jasmine: {
       src: 'src/**/*.js',
       options: {
         specs: 'spec/**/*.js',
+        helpers: ['lib/jasmine-2.0.0/jquery.js', 'lib/jasmine-2.0.0/sinon.js'],
         template: require('grunt-template-jasmine-requirejs'),
         templateOptions: {
           requireConfig: {
